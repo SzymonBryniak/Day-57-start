4441993d7e67a5fbc8eaed33903a93fd0867ba81
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2025-01-18 21:57:53 +0000

    Day 57 progress

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import random
+import requests
 from datetime import date
+import time
 app = Flask(__name__)
 
 
@@ -9,6 +11,15 @@ def home():
   random_number = random.randint(1, 10)
   year = date.today().year
   return render_template("index.html", num=random_number, year=year)
+
+@app.route('/guess/<string:name>')
+def guess(name):
+  params = {
+    "name": name
+  }
+  age = requests.get('https://api.agify.io?', params=params)
+  return f'Hey {name.capitalize()}\
+   You are maybe {age['name']} '
 
 
 if __name__ == "__name__":
